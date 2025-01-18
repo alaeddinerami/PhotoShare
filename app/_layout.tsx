df63ab84fd179cbe56bfacb8c6c4e1d34719596a
alaeddinerami <alaeddinerami7@gmail.com>
2025-01-18 18:22:48 +0100

    feat: setup the project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Slot, Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
+import "../global.css";
+
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -29,10 +31,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+     <Slot/>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
---
+++ components/auth/SignUp.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+import "../../global.css";
+
+const SignUp: React.FC = ()=>{
+    return(
+        <View className="bg-blue-900">
+            <Text className="text-red-700">lets begsdvmnin</Text>
+        </View>
+    )
+}
+export default SignUp;
